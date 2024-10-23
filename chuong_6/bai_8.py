40ed4ce82d26cde844dd07426b295ff49eccea1f
Tran_Duc_Co <tdco.24174600072@sv.uneti.edu.com>
2024-10-23 21:27:36 +0700

    bai 2

--- chuong_6/bai_8.py
+++
@@ -1,7 +0,0 @@
-import math 
-x = float(input("Nhập giá trị x: "))
-if x > 0 and x != 1:
-    G = math.log(4) / math.log(x) + math.log(2) / math.log(x)
-    print(f"Giá trị của G là: {G:4.2f}")
-else: 
-    print("Bạn chưa nhập đúng x để thỏa mãn điều kiện ")
